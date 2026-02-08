d121cb5a26d31983004a340224f54f9cd1c6eb2a
Aarush Parikh <parikhaarush@gmail.com>
2026-02-07 22:57:28 -0800

    fix to main

--- test/src/Main.java
+++ test/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     /**
      * Main method that starts the image processing application.
      * 
-     * @param args command line arguments (not used)
      * @precondition none
      * @postcondition MenuDriver is initialized and runMenu() is called to start the interactive menu loop
      */
